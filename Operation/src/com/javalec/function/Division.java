a8d4d9a92ae3eb2788f362bee84ceb33ee529967
Gwangyeong Kim <rhkddud0310@naver.com>
2023-12-05 16:02:12 +0900

    first commit

--- Operation/src/com/javalec/function/Division.java
+++ Operation/src/com/javalec/function/Division.java
@@ -1,9 +1,20 @@
 package com.javalec.function;
 
-public class Division {// 광영
+public class Division {
+	// 숫자 3개를 나눈 뒤 Main class에 출력되게 할 것.
+	
 	// Field
 	
 	// Constructor
+	public Division() {
+	}
 		
 	// Method
+	// 숫자 3개를 나누기.
+	public int divCalc(int num1, int num2, int num3) {
+		int result = num1/num2/num3;
+		return result;
+	}
+	
+	
 }
