86c8fd2d0532e3ca73caf839e259a69ba8fffd75
ssujjy <ssujjy@gmail.com>
2023-12-05 15:20:57 +0900

    first commit

--- Operation/src/com/javalec/function/Multiplication.java
+++ Operation/src/com/javalec/function/Multiplication.java
@@ -4,6 +4,18 @@ public class Multiplication {// 수진
 	// Field
 	
 	// Constructor
-		
+	public Multiplication() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	// Method
+	public int calMul(int num1, int num2, int num3) {
+		int result = 0;
+		
+		result = num1*num2*num3;
+		
+		return result;
+		
+		
+	}
 }
