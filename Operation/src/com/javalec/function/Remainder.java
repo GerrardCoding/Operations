2505eb344ea896a7654b2ccccd7540cf7e68f60f
DaegeunLee <foejakzm@naver.com>
2023-12-05 16:22:05 +0900

    third commit

--- Operation/src/com/javalec/function/Remainder.java
+++ Operation/src/com/javalec/function/Remainder.java
@@ -4,6 +4,14 @@ public class Remainder {// 대근
 	// Field
 	
 	// Constructor
+	public Remainder() {
 		
+	}
 	// Method
+	public int[] Remainder(int num1, int num2, int num3) {
+		int[] result = new int[2];
+		result[0] = num1 % num2;
+		result[1] = num2 % num3;
+		return result;
+	}
 }
