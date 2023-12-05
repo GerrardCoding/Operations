43cd1f8b6bb8e1f448dc5d13f32cfd5698284919
Sumin Kim <411sumin@gmail.com>
2023-12-05 16:19:51 +0900

    werefwre

--- Operation/src/com/javalec/function/Subtraction.java
+++ Operation/src/com/javalec/function/Subtraction.java
@@ -4,6 +4,17 @@ public class Subtraction {// 수민
 	// Field
 	
 	// Constructor
+	public Subtraction() {
+		
+	}
+	
+
 		
 	// Method
+	public int Subtraction (int num1, int num2, int num3) {
+		int result = (num1 - num2 - num3);
+		return result;
+	}
+	
+	
 }
